ed753da406258d890c055df87b33357f2b524925
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2017-05-28 14:54:04 +0530

    Port changed

--- Hardware/Blinking/main.c
+++ Hardware/Blinking/main.c
@@ -6,7 +6,7 @@ void main(void)
 DDRC=0xFF;                //declaring PORTC as output
 while(1)                  //infinite loop
 {
-PORTB=0xFF;             //making all pins of PORTC HIGH
+PORTC=0xFF;             //making all pins of PORTC HIGH
 _delay_ms(1000);        //delay of 1000 ms
 PORTC=0x00;             //making all pins of PORTC LOW
 _delay_ms(1000);         //delay of 1000 ms
